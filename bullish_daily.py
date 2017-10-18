6b02792aa322191238ebe013bc998ea6d6a8670b
Zhiyuan <zzou@linkedin.com>
2017-10-17 22:15:40 -0700

    add daily bullish

--- bullish_daily.py
+++ bullish_daily.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 19:06:16 2017
+
+@author: zzou
+"""
+
 from alpha_vantage.techindicators import TechIndicators
 from urllib2 import HTTPError
 import time
 import numpy as np
 from alpha_vantage.timeseries import TimeSeries
 
-text_file = open("symbols.txt", "r")
+text_file = open("weekly.sel", "r")
 symbols = text_file.read().split()
-bullish_weekly = []
+bullish_daily = []
 #ti = TechIndicators(key = 'MKAV2AXSQSPRE2R0',output_format = 'pandas')
 #for s in symbols:
 #    try:
@@ -24,19 +32,19 @@ bullish_weekly = []
 ts = TimeSeries(key = 'MKAV2AXSQSPRE2R0',output_format = 'pandas')
 for s in symbols:
     try:
-        data, meta_data = ts.get_weekly(symbol = s)
+        data, meta_data = ts.get_daily_adjusted(symbol = s)
         close_price = data["close"]
+        print close_price
         sma10 = np.mean(close_price[-10:-1])
         sma15 = np.mean(close_price[-15:-1])
         sma20 = np.mean(close_price[-20:-1])
         if (sma10 > sma15 and sma15 > sma20):
-            bullish_weekly.append(s)
+            bullish_daily.append(s)
     except HTTPError:
         time.sleep(5)
         continue
  
-output_file = open("output.sel", "w")
+output_file = open("daily.sel", "w")
 output_file.write("\n".join(bullish_weekly))
 text_file.close()
 output_file.close()
-
---
+++ bullish_weekly.py
@@ -0,0 +1,42 @@
+from alpha_vantage.techindicators import TechIndicators
+from urllib2 import HTTPError
+import time
+import numpy as np
+from alpha_vantage.timeseries import TimeSeries
+
+text_file = open("symbols.txt", "r")
+symbols = text_file.read().split()
+bullish_weekly = []
+#ti = TechIndicators(key = 'MKAV2AXSQSPRE2R0',output_format = 'pandas')
+#for s in symbols:
+#    try:
+#        data, meta_data = ti.get_sma(symbol = s, interval = 'weekly', time_period = 10, series_type = 'close')
+#        sma10 = data['SMA'][-1]
+#        data, meta_data = ti.get_sma(symbol = s, interval = 'weekly', time_period = 15, series_type = 'close')
+#        sma15 = data['SMA'][-1]
+#        data, meta_data = ti.get_sma(symbol = s, interval = 'weekly', time_period = 20, series_type = 'close')
+#        sma20 = data['SMA'][-1]
+#        if (sma10 > sma15 and sma15 > sma20):
+#            bullish_weekly.append(s)
+#    except HTTPError:
+#        time.sleep(30)
+
+ts = TimeSeries(key = 'MKAV2AXSQSPRE2R0',output_format = 'pandas')
+for s in symbols:
+    try:
+        data, meta_data = ts.get_weekly(symbol = s)
+        close_price = data["close"]
+        sma10 = np.mean(close_price[-10:-1])
+        sma15 = np.mean(close_price[-15:-1])
+        sma20 = np.mean(close_price[-20:-1])
+        if (sma10 > sma15 and sma15 > sma20):
+            bullish_weekly.append(s)
+    except HTTPError:
+        time.sleep(5)
+        continue
+ 
+output_file = open("weekly.sel", "w")
+output_file.write("\n".join(bullish_weekly))
+text_file.close()
+output_file.close()
+
